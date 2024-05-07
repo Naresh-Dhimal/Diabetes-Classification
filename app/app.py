3c89de9b88ce23e33ceffe3648eddc56cfb8ff30
Naresh Dhimal <nareshdhimal72@gmail.com>
2024-05-07 22:09:58 +0545

    Streamlit app

--- app/app.py
+++ app/app.py
@@ -4,10 +4,10 @@ import streamlit as st
 # deserializing pickle file
 def loading_model():
     try:
-        with open("../notebooks/final_model.pickle","br") as file:
+        with open("app/final_model.pickle","br") as file:
             model = pickle.load(file)
         return model
-    except FileNotFoundError as e:
+    except FileNotFoundError as e: 
         st.write(e)
         
 
@@ -22,11 +22,11 @@ def main():
 
         col3, col4, col5 = st.columns(3)
         with col3:
-            blood_pressure = st.text_input("Diastolic blood pressure (mm Hg)")
+            blood_pressure = st.text_input("BloodPressure")
         with col4:
-            skin_thickness = st.text_input("Triceps skin fold thickness (mm)")
+            skin_thickness = st.text_input("SkinThickness")
         with col5:
-            insulin = st.text_input("serum insulin (mu U/ml)")
+            insulin = st.text_input("Insulin")
         col6, col7, col8 = st.columns(3)
         with col6:
             bmi = st.text_input("Body mass index")
@@ -35,7 +35,16 @@ def main():
         with col8:
             age = st.text_input("Age (years)")
 
-        
+       
+        col100, col200, c0l300 = st.columns(3)
+        if col200.button("Classify"):
+            pass
+            model = loading_model()
+            classified = model.predict([[float(pregnancies), float(glucose), float(blood_pressure), float(skin_thickness), float(insulin), float(bmi), float(diabetes_pedigree_function), float(age)]])
+            if classified[0] == 1:
+                st.write("The patient is predicted to have diabetes.")
+            else:
+                st.write("The patient is predicted to not have diabetes.")
     except ValueError as e:
         st.write(e)
 if __name__ == "__main__":
